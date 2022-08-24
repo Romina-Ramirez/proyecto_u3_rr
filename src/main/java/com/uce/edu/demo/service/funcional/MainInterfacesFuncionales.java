bf8c32e1a82cd4fc67d4fe7e4c183dce9627b1e8
Romina-Ramirez <romina.maite.rt@gmail.com>
2022-08-23 21:08:49 -0500

    Taller 34 - Interfaces Funcionales y Métodos High Order

--- src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
+++ src/main/java/com/uce/edu/demo/service/funcional/MainInterfacesFuncionales.java
@@ -8,6 +8,8 @@ public class MainInterfacesFuncionales {
 
 	public static void main(String[] args) {
 
+		ConsumoMetodosHighOrder metodosHighOrder = new ConsumoMetodosHighOrder();
+
 // 1. Supplier
 //		Clases
 		IPersonaSupplier<String> supplierClase = new PersonaSupplierImpl();
@@ -22,29 +24,66 @@ public class MainInterfacesFuncionales {
 
 		IPersonaSupplier<String> supplierLambdaTE = () -> "Daniel TE2";
 		logger.info("SupplierTE Lambda: " + supplierLambdaTE.getNombre());
-		
+
+// 		Métodos High Order
+		String valorHO = metodosHighOrder.consumirSupplier(() -> "Hola mundo");
+		logger.info("Supplier High Order: " + valorHO);
+
+		String valorHO2 = metodosHighOrder.consumirSupplier(() -> {
+			String valorConsultado = "1723069801";
+			return valorConsultado;
+		});
+		logger.info("Supplier High Order2: " + valorHO2);
+
 // 		JAVA
 
 // 2. Consumer
 // 		Clases
 		IPersonaConsumer<String> consumerClase = new PersonaConsumerImpl();
-		consumerClase.accept("Prueba Consumer Clase");
+		logger.info("Consumer Clase");
+		consumerClase.accept("Prueba");
 
 // 		Lambdas
 		IPersonaConsumer<String> consumerLambda = cadena -> System.out.println(cadena);
-		consumerLambda.accept("Prueba Consumer Lambda");
+		logger.info("Consumer Lambda");
+		consumerLambda.accept("Prueba");
+
+// 		Métodos High Order
+		logger.info("Consumer High Order");
+		metodosHighOrder.consumirConsumer(valor -> System.out.println(valor), 2);
 
 // 3. Predicate
-// 		Clases
 // 		Lambdas
+		IPersonaPredicate<String> predicateLambda = cadena -> cadena.contains("Z");
+		logger.info("Predicate Lambda: " + predicateLambda.evaluar("RAMÍREZ"));
+
+// 		Métodos High Order
+		boolean respuesta = metodosHighOrder.consumirPredicate(cadena -> cadena.contains("O"), "RAMÍREZ");
+		logger.info("Predicate High Order: " + respuesta);
 
 // 4. Function
-// 		Clases
 // 		Lambdas
+		IPersonaFunction<Integer, String> functionLambda = cadena -> {
+			Integer valor = Integer.parseInt(cadena);
+			Integer valorFinal = valor - 2;
+			return valorFinal;
+		};
+		logger.info("Function Lambda: " + functionLambda.aplicar("7"));
+
+// 		Métodos High Order
+		String valorFinalHO = metodosHighOrder.consumirFunction(valor -> {
+			String retorn = valor.toString() + "A";
+			return retorn;
+		}, 1);
+		logger.info("Function High Order: " + valorFinalHO);
 
 // 5. UnaryOperator (Function)
-// 		Clases
 // 		Lambdas
+		IPersonaUnaryOperator<String> unaryLambda = cadena -> {
+			String valorFinal = cadena.concat(" Ramírez");
+			return valorFinal;
+		};
+		logger.info("Unary Lambda: " + unaryLambda.apply("Romina"));
 
 	}
 
